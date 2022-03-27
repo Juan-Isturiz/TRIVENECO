3b848a10c5aeb35c320980d2f762ee17bebfe9a4
andyalv5 <andyalvarez@correo.unimet.edu.ve>
2022-03-27 12:39:59 -0400

    exagerando

--- src/Components/UploadData/UploadHotel.jsx
+++ src/Components/UploadData/UploadHotel.jsx
@@ -180,12 +180,13 @@ export default function UploadData() {
    		    </select>
 
                <h3 className={styles.h3}>Seleccione la ciudad:</h3>
-               {docusCity.map((doc)=><li key={doc.keyCode}>
                <select onChange={(e) => handleChange(e)} name="ciudades">
-                   <option value={doc.nombre}>{doc.nombre}</option>
-                   </select>
-               </li>)}
+               {docusCity.map((doc)=>
                
+                   <option value={doc.nombre}>{doc.nombre}</option>
+                   
+              )}
+               </select>
             
                       
    		   
