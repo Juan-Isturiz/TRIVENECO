13b4b365f3e03583ce6b99e30ba5a5de3edc2307
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2022-03-27 11:07:55 -0400

    Fixing app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Routering from "./Routering";
 import UserContextProvider from "./Context/Context";
 import { BrowserRouter as Router } from "react-router-dom";
-import NavBar from "./Components/NavBar/Navbar";
+import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
