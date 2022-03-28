82f364f7f35dbb1623b10a01ab9aa62c33a545b2
nicoabb <nicolas.briceno@correo.unimet.edu.ve>
2022-03-28 00:58:46 -0400

    Fix details with LandPage

--- src/Components/Particular/HotelParticular.jsx
+++ src/Components/Particular/HotelParticular.jsx
@@ -5,11 +5,10 @@ import styles from "./Particular.module.css";
 import Reservation from "../Reservation/ReservationGen";
 import { Link, useNavigate } from "react-router-dom";
 
-
-function searchingTerm(id){
-    return function(x){
-        return x.keyCode.toLowerCase().includes(id) || !id
-    }
+function searchingTerm(id) {
+    return function (x) {
+        return x.keyCode.toLowerCase().includes(id) || !id;
+    };
 }
 
 export function HotelParticular() {
@@ -34,42 +33,50 @@ export function HotelParticular() {
                         text={doc.descripcion}
                         logo={false}
                     ></Header>
-                    <h1 className={styles.titulo}>Casino: </h1>
-                    <h3 className={styles.descripcion}>{doc.Casino}</h3>
-                    <h1 className={styles.titulo}>Desayuno: </h1>
-                    <h3 className={styles.descripcion}>{doc.Comida}</h3>
-                    <h1 className={styles.titulo}>Playa: </h1>
-                    <h3 className={styles.descripcion}>{doc.Playa}</h3>
-                    <h1 className={styles.titulo}>Ciudad: </h1>
-                    <h3 className={styles.descripcion}>{doc.ciudad}</h3>
-                    <h1 className={styles.titulo}>Entretenimiento: </h1>
-                    <h3 className={styles.descripcion}>
-                        {doc.entretenimiento}
-                    </h3>
-                    <h1 className={styles.titulo}>Se permiten mascotas: </h1>
-                    <h3 className={styles.descripcion}>{doc.mascota}</h3>
+                    <div className={styles.container}>
+                        <h1 className={styles.titulo}>Casino: </h1>
+                        <h3 className={styles.descripcion}>{doc.Casino}</h3>
+                        <h1 className={styles.titulo}>Desayuno: </h1>
+                        <h3 className={styles.descripcion}>{doc.Comida}</h3>
+                        <h1 className={styles.titulo}>Playa: </h1>
+                        <h3 className={styles.descripcion}>{doc.Playa}</h3>
+                        <h1 className={styles.titulo}>Ciudad: </h1>
+                        <h3 className={styles.descripcion}>{doc.ciudad}</h3>
+                        <h1 className={styles.titulo}>Entretenimiento: </h1>
+                        <h3 className={styles.descripcion}>
+                            {doc.entretenimiento}
+                        </h3>
+                        <h1 className={styles.titulo}>
+                            Se permiten mascotas:{" "}
+                        </h1>
+                        <h3 className={styles.descripcion}>{doc.mascota}</h3>
 
-                    <h1 className={styles.titulo}>Intalaciones: </h1>
-                    <h3 className={styles.titulo}>{doc.lugar}</h3>
-                    <img src={doc.url2} height="500px" width="600px"></img>
-                    <h1 className={styles.titulo}>Descripción</h1>
+                        <h1 className={styles.titulo}>Intalaciones: </h1>
+                        <h3 className={styles.titulo}>{doc.lugar}</h3>
+                        <img src={doc.url2} height="500px" width="600px"></img>
+                        <h1 className={styles.titulo}>Descripción</h1>
 
-                    <h3 className={styles.descripcion}>{doc.descripcion2}</h3>
+                        <h3 className={styles.descripcion}>
+                            {doc.descripcion2}
+                        </h3>
 
-                    <h3 className={styles.titulo}>{doc.lugar2}</h3>
-                    <img src={doc.url3} height="500px" width="600px"></img>
-                    <h1 className={styles.titulo}>Descripción</h1>
+                        <h3 className={styles.titulo}>{doc.lugar2}</h3>
+                        <img src={doc.url3} height="500px" width="600px"></img>
+                        <h1 className={styles.titulo}>Descripción</h1>
 
-                    <h3 className={styles.descripcion}>{doc.descripcion3}</h3>
+                        <h3 className={styles.descripcion}>
+                            {doc.descripcion3}
+                        </h3>
 
-                    <h3 className={styles.h3}>Seleccione la habitación:</h3>
-                    {doc.lista2.map((docu) => (
-                        <div key={docu.keyCode2}>
-                            <Link to={`/Reserva/${docu.keyCode2}`}>
-                                {docu.habitacion}
-                            </Link>
-                        </div>
-                    ))}
+                        <h3 className={styles.h3}>Seleccione la habitación:</h3>
+                        {doc.lista2.map((docu) => (
+                            <div key={docu.keyCode2}>
+                                <Link to={`/Reserva/${docu.keyCode2}`}>
+                                    {docu.habitacion}
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
                 </li>
             ))}
 
